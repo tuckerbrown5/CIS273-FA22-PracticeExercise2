adc8d117111745ca7a2f57d30488e4e09dc96601
Kenan Casey <kenancasey@gmail.com>
2022-10-04 09:26:35 -0500

    initial commit

---
+++ PracticeExercise2/LinkedList.cs
@@ -0,0 +1,127 @@
+﻿using System;
+namespace PracticeExercise2
+{
+
+    public class LinkedListNode<T>
+    {
+        public T Data { get; set; }
+        public LinkedListNode<T> Next { get; set; }
+
+        public LinkedListNode(T data = default(T), LinkedListNode<T> next=null)
+        {
+            Data = data;
+            Next = next;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+
+    }
+
+	public class LinkedList<T>: IList<T>
+	{
+        public LinkedListNode<T> Head { get; set; }
+        public LinkedListNode<T> Tail { get; set; }
+
+		public LinkedList()
+		{
+            Head = null;
+            Tail = null;
+		}
+
+        public T? First => throw new NotImplementedException();
+
+        public T? Last => throw new NotImplementedException();
+
+        public bool IsEmpty => throw new NotImplementedException();
+
+        public int Length => throw new NotImplementedException();
+
+        public void Append(T value)
+        {
+
+            var newNode = new LinkedListNode<T>(value);
+
+            if (IsEmpty)
+            {
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                Tail.Next = newNode;
+                Tail = newNode;
+            }
+
+
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int FirstIndexOf(T value)
+        {
+            int index = 0;
+
+            var currentNode = Head;
+
+            while(currentNode!=null)
+            {
+                if( currentNode.Data.Equals(value) )
+                {
+                    return index;
+                }
+                index++;
+                currentNode = currentNode.Next;
+
+            }
+
+            return -1;
+        }
+
+        public T Get(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertAfter(T newValue, int existingValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertAt(T value, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Prepend(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> Reverse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ UnitTests/LinkedListUnitTests.cs
@@ -0,0 +1,369 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeExercise2;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class LinkedListUnitTests
+    {
+        [TestMethod]
+        public void TestLength()
+        {
+
+            IList<int> list = new LinkedList<int>();
+
+            Assert.AreEqual(0, list.Length);
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Append(i);
+            }
+
+            Assert.AreEqual(10, list.Length);
+
+            for (int i = 0; i < 32; i++)
+            {
+                list.Append(i);
+            }
+
+            Assert.AreEqual(42, list.Length);
+
+        }
+
+        [TestMethod]
+        public void TestIsEmpty()
+        {
+
+            IList<int> list = new LinkedList<int>();
+
+            Assert.IsTrue(list.IsEmpty);
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Append(i);
+            }
+
+            Assert.IsFalse(list.IsEmpty);
+
+        }
+
+        [TestMethod]
+        public void TestFirst()
+        {
+            IList<int> list = new LinkedList<int>();
+
+            Assert.ThrowsException<NullReferenceException>(() =>
+            {
+                int i = list.First;
+            });
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Append(i);
+                Assert.AreEqual(0, list.First);
+            }
+
+            Assert.AreEqual(10, list.Length);
+
+            for (int i = 0; i < 32; i++)
+            {
+                list.Append(i);
+                Assert.AreEqual(0, list.First);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestLast()
+        {
+            IList<int> list = new LinkedList<int>();
+
+            Assert.ThrowsException<NullReferenceException>(() =>
+            {
+                int i = list.Last;
+            });
+
+            list.Append(0);
+
+            Assert.AreEqual(0, list.Last);
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Append(i);
+                Assert.AreEqual(i, list.Last);
+            }
+
+            for (int i = 0; i < 32; i++)
+            {
+                list.Append(i);
+                Assert.AreEqual(i, list.Last);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestAppend()
+        {
+            IList<int> list = new LinkedList<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Append(i);
+                Assert.AreEqual(i, list.Last);
+            }
+
+            Assert.AreEqual("[0,1,2,3,4,5,6,7,8,9]", list.ToString().Replace(" ", ""));
+            Assert.AreEqual(10, list.Length);
+
+            for (int i = 0; i < 32; i++)
+            {
+                list.Append(i);
+                Assert.AreEqual(i, list.Last);
+            }
+
+            Assert.AreEqual("[0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]", list.ToString().Replace(" ", ""));
+
+        }
+
+        [TestMethod]
+        public void TestPrepend()
+        {
+            IList<int> list = new LinkedList<int>();
+            list.Prepend(0);
+            Assert.AreEqual(0, list.First);
+
+            list = new LinkedList<int>();
+
+            for (int i = 9; i >= 0; i--)
+            {
+                list.Prepend(i);
+                Assert.AreEqual(i, list.First);
+            }
+
+            Assert.AreEqual("[0,1,2,3,4,5,6,7,8,9]", list.ToString().Replace(" ", ""));
+            Assert.AreEqual(10, list.Length);
+
+            for (int i = 31; i >= 0; i--)
+            {
+                list.Prepend(i);
+                Assert.AreEqual(i, list.First);
+            }
+
+            Assert.AreEqual("[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,0,1,2,3,4,5,6,7,8,9]", list.ToString().Replace(" ", ""));
+
+        }
+
+        [TestMethod]
+        public void TestInsertAfter()
+        {
+            IList<int> list = new LinkedList<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Append(i);
+            }
+
+            list.InsertAfter(42, 0);
+            Assert.AreEqual("[0,42,1,2,3,4]", list.ToString().Replace(" ", ""));
+
+            list.InsertAfter(42, 3);
+            Assert.AreEqual("[0,42,1,2,3,42,4]", list.ToString().Replace(" ", ""));
+
+            list.InsertAfter(42, 4);
+            Assert.AreEqual("[0,42,1,2,3,42,4,42]", list.ToString().Replace(" ", ""));
+
+            list.InsertAfter(42, 42);
+            Assert.AreEqual("[0,42,42,1,2,3,42,4,42]", list.ToString().Replace(" ", ""));
+
+            list.InsertAfter(400, 8);
+            Assert.AreEqual("[0,42,42,1,2,3,42,4,42,400]", list.ToString().Replace(" ", ""));
+
+            list = new LinkedList<int>();
+            list.InsertAfter(42, 3);
+            Assert.AreEqual("[42]", list.ToString().Replace(" ", ""));
+        }
+
+        [TestMethod]
+        public void TestInsertAt()
+        {
+            IList<int> list = new LinkedList<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Append(i);
+                Assert.AreEqual(i, list.Last);
+            }
+
+            list.InsertAt(42, 0);
+            Assert.AreEqual("[42,0,1,2,3,4]", list.ToString().Replace(" ", ""));
+
+            list.InsertAt(52, 3);
+            Assert.AreEqual("[42,0,1,52,2,3,4]", list.ToString().Replace(" ", ""));
+
+            list.InsertAt(62, list.Length - 1);
+            Assert.AreEqual("[42,0,1,52,2,3,62,4]", list.ToString().Replace(" ", ""));
+
+            list.InsertAt(72, list.Length );
+            Assert.AreEqual("[42,0,1,52,2,3,62,4,72]", list.ToString().Replace(" ", ""));
+
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() =>
+            {
+                list.InsertAt(42, 42);
+            });
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() =>
+            {
+                list.InsertAt(42, -5);
+            });
+
+            list = new LinkedList<int>();
+            list.InsertAt(42, 0);
+            Assert.AreEqual("[42]", list.ToString().Replace(" ", ""));
+
+            list.InsertAt(52, 1);
+            Assert.AreEqual("[42,52]", list.ToString().Replace(" ", ""));
+            Assert.AreEqual(52, list.Last);
+        }
+
+        [TestMethod]
+        public void TestFirstIndexOf()
+        {
+            IList<int> list = new LinkedList<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Append(i);
+            }
+
+            Assert.AreEqual(0, list.FirstIndexOf(0));
+            Assert.AreEqual(4, list.FirstIndexOf(4));
+            Assert.AreEqual(-1, list.FirstIndexOf(10));
+
+            for (int i = 0; i < 50; i++)
+            {
+                list.Append(i);
+            }
+
+            Assert.AreEqual(0, list.FirstIndexOf(0));
+            Assert.AreEqual(4, list.FirstIndexOf(4));
+            Assert.AreEqual(15, list.FirstIndexOf(10));
+        }
+
+        [TestMethod]
+        public void TestRemove()
+        {
+            IList<int> list = new LinkedList<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Append(i);
+            }
+
+            list.Remove(0);
+            Assert.AreEqual("[1,2,3,4]", list.ToString().Replace(" ", ""));
+
+            list.Remove(2);
+            Assert.AreEqual("[1,3,4]", list.ToString().Replace(" ", ""));
+
+            list.Remove(4);
+            Assert.AreEqual("[1,3]", list.ToString().Replace(" ", ""));
+
+            list.Remove(5);
+            Assert.AreEqual("[1,3]", list.ToString().Replace(" ", ""));
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Append(i);
+            }
+            Assert.AreEqual("[1,3,0,1,2,3,4,5,6,7,8,9]", list.ToString().Replace(" ", ""));
+
+            list.Remove(1);
+            list.Remove(1);
+            Assert.AreEqual("[3,0,2,3,4,5,6,7,8,9]", list.ToString().Replace(" ", ""));
+
+            // Empty list
+            list = new LinkedList<int>();
+            list.Remove(3);
+            Assert.AreEqual("[]", list.ToString().Replace(" ", ""));
+
+        }
+
+        [TestMethod]
+        public void TestRemoveAt()
+        {
+            IList<int> list = new LinkedList<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Append(i);
+                Assert.AreEqual(i, list.Last);
+            }
+
+            list.RemoveAt(0);
+            Assert.AreEqual("[1,2,3,4]", list.ToString().Replace(" ", ""));
+
+            list.RemoveAt(list.Length - 1);
+            Assert.AreEqual("[1,2,3]", list.ToString().Replace(" ", ""));
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() =>
+            {
+                list.RemoveAt(list.Length);
+            });
+
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() =>
+            {
+                list.RemoveAt(42);
+            });
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() =>
+            {
+                list.RemoveAt(-5);
+            });
+
+            list = new LinkedList<int>();
+            Assert.ThrowsException<IndexOutOfRangeException>(() =>
+            {
+                list.RemoveAt(0);
+            });
+
+        }
+
+        [TestMethod]
+        public void TestClear()
+        {
+            IList<int> list = new LinkedList<int>();
+            list.Clear();
+            Assert.AreEqual(0, list.Length);
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Append(i);
+            }
+
+            list.Clear();
+            Assert.AreEqual(0, list.Length);
+
+            for (int i = 0; i < 64; i++)
+            {
+                list.Append(i);
+                Assert.AreEqual(i, list.Last);
+            }
+            list.Clear();
+            Assert.AreEqual(0, list.Length);
+        }
+
+        [TestMethod]
+        public void TestReverse()
+        {
+            IList<int> list = new LinkedList<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Append(i);
+            }
+
+            var reversed = list.Reverse();
+            Assert.AreEqual("[4,3,2,1,0]", reversed.ToString().Replace(" ", ""));
+            Assert.AreEqual("[0,1,2,3,4]", list.ToString().Replace(" ", ""));
+
+            Assert.AreEqual("[0,1,2,3,4]", reversed.Reverse().ToString().Replace(" ", ""));
+        }
+    }
+}
+
